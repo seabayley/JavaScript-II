6b3bf0f4da970d89f1d2851f65a16bc756cddf2c
Christopher Bailey <seabayley@outlook.com>
2019-08-20 17:10:29 -0500

    Finished up stretches

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,18 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const myFun = function () {
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
+  return function () { count += 1; return count; }
   // Return a function that when invoked increments and returns a counter variable.
-};
+}
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -18,4 +22,12 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
+  let count = 0;
+
+  const counter = {
+    increment: function () { count += 1; return count; },
+    decrement: function () { count -= 1; return count; }
+  }
+  return counter;
 };
